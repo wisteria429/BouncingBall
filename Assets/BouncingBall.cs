bd157688433429b30a08c6a8e46f9f8f1cc8da16
wisteria429 <wisteriaroots429@gmail.com>
2017-05-01 19:15:36 +0900

    create app

---
+++ Assets/BouncingBall.cs
@@ -0,0 +1,38 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Windows.Speech;
+
+public class BouncingBall : MonoBehaviour, IInputClickHandler {
+    public GameObject pubfubBall;
+
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("click");
+        Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
+        
+        GameObject ball = Instantiate(pubfubBall);
+        ball.transform.position = Camera.main.transform.TransformPoint(0, 0, 1.2f);
+        Rigidbody ballRigitBody = ball.GetComponent<Rigidbody>();
+        Debug.Log("forward:" + forward.ToString());
+
+        // カメラで見ている方向に移動量を追加
+        ballRigitBody.AddForce(forward * 5, ForceMode.Impulse);
+        
+    }
+
+}
---
+++ Assets/GlobalControll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class GlobalControll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void resetBall() {
+        GameObject[] balls = GameObject.FindGameObjectsWithTag("ball");
+
+        foreach (GameObject ball in balls) {
+            Debug.Log("Destroy ball");
+
+            Destroy(ball);
+
+        }
+    }
+}
