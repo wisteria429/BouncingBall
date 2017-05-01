4aec921f332c3b9f95d2624ab7a61e6d614ea371
wisteria429 <wisteriaroots429@gmail.com>
2017-05-01 19:46:08 +0900

    create TextControll

---
+++ Assets/TextControll.cs
@@ -0,0 +1,27 @@
+﻿using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextControll : MonoBehaviour {
+
+    public SpatialMappingManager mapping;
+
+    private bool isStart = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isStart && mapping.GetMeshes().Count != 0) {
+            // SpatialMappingのメッシュが生成されたら1度だけ通る
+            isStart = true;
+            TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+            textMesh.text = "AirTapでボールを発射！";
+        }
+
+            
+	}
+}
